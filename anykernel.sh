9e7daa0f322694e28643f52b0a86c807ccd061bf
wwlwwlwwlwwl <47500260+wwlwwlwwlwwl@users.noreply.github.com>
2023-01-02 16:00:50 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -6,8 +6,8 @@
 properties() { '
 kernel.string=VenusKernel With Docker by wwlwwl
 do.devicecheck=1
-do.modules=1
-do.systemless=1
+do.modules=0
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=venus
